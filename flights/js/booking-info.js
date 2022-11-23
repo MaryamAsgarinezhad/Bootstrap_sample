a6dfcd909d7c8d469208d0413ec5cb02857da4b0
Mary Asgari <maryamasgn123@gmail.com>
2022-11-23 17:59:40 +0330

    function added

---
+++ flights/js/booking-info.js
@@ -0,0 +1,24 @@
+'use strict';
+var passengerInfoNumber=1;
+var input = "salaaaam";
+
+othername();
+
+function othername() {
+    
+    input = document.getElementsByClassName('safa').value;
+    input="dad";
+    alert(input);
+}
+
+function getInfo() {
+        document.getElementById('infoNumber').innerHTML="Enter info for passenger #"+input;
+    // if(passengerInfoNumber<input){
+    //     passengerInfoNumber=passengerInfoNumber+1;
+
+        
+    // }
+    // else{
+
+    // }
+}
