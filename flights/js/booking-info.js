0c16bb9003d64ab87dd688a80fa4bf4e3ac9ace0
piff-saad <pooriya.safaei.80@gmail.com>
2022-11-30 01:46:43 -0800

    sssss

--- flights/js/booking-info.js
+++ flights/js/booking-info.js
@@ -1,25 +1,32 @@
 'use strict';
-var passengerInfoNumber=1;
-var input = "salaaaam";
 
-othername();
+let num=1;
 
-function othername() {
-    
-    input = document.getElementsByClassName('safa').value;
-    input="dad";
-    alert(input);
+function book(firstname , lastname , passportnumber){
+    //todo
+    console.log("purchased for "+ firstname+lastname +" "+passportnumber);
+    document.getElementById('infoNumber').innerHTML ="Enter information for passenger " +num;
 }
 
-function getInfo() {
-        document.getElementById('infoNumber').innerHTML="Enter info for passenger #"+input;
-    // if(passengerInfoNumber<input){
-    //     passengerInfoNumber=passengerInfoNumber+1;
+function getInfo(){
+    if (num < 5){
+        let firstname = document.getElementById('firstname').value;
+        let lastname = document.getElementById('lastname').value;
+        let passportnumber = document.getElementById('passportnumber').value;
+        let message = document.getElementById('infoNumber');
 
-        
-    // }
-    // else{
-
-    // }
+        if(!!firstname && !!lastname && !isNaN(passportnumber) && !isNaN(parseFloat(passportnumber))){
+            event.preventDefault();
+            event.stopPropagation();
+            book(firstname , lastname , passportnumber);
+            num++;
+        }
+        else{
+            message.innerHTML = "Fill all fields!";
+        }
+    }
+    else{
+        console.log('done!');
+    }
 }
 
--- login/js/my-login.js
+++ login/js/my-login.js
@@ -1,20 +1,3 @@
-/******************************************
- * My Login
- *
- * Bootstrap 4 Login Page
- *
- * @author          Muhamad Nauval Azhar
- * @uri 			https://nauval.in
- * @copyright       Copyright (c) 2018 Muhamad Nauval Azhar
- * @license         My Login is licensed under the MIT license.
- * @github          https://github.com/nauvalazhar/my-login
- * @version         1.2.0
- *
- * Help me to keep this project alive
- * https://www.buymeacoffee.com/mhdnauvalazhar
- * 
- ******************************************/
-
 'use strict';
 
 $(function() {
@@ -37,7 +20,7 @@ $(function() {
 			paddingRight: 60
 		});
 		$this.after($("<div/>", {
-			html: 'Show',
+			html: 'نمایش',
 			class: 'btn btn-primary btn-sm',
 			id: 'passeye-toggle-'+i,
 		}).css({
